06d152536aad1e17ce75b7e5a4eac8266ace750e
Luiz Palma <luizpalma16@gmail.com>
2026-06-09 21:09:20 -0300

    Iniciando projeto com Clean Arch

---
+++ backend/Planilharias.Domain/Workbooks/Workbook.cs
@@ -0,0 +1,16 @@
+﻿using Planilharias.Domain.Sheets;
+
+namespace Planilharias.Domain.Workbooks;
+
+public class Workbook
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public List<Sheet> sheets { get; set; } = [];
+}
